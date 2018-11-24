55c5cb3bbab132bc231a58746fc7aaff7a0ca3da
eboy <giannisduke@gmail.com>
2018-11-24 10:50:22 +0200

    eboy: logo and hero text padding on mobile

--- site/web/app/themes/eboy/templates/header.php
+++ site/web/app/themes/eboy/templates/header.php
@@ -1,6 +1,5 @@
 
-	<header class="d-flex flex-row justify-content-between px-5 py-3">
-
+	<header class="d-flex flex-row justify-content-between align-items-center px-5 py-3">
   <div class="col-6 col-lg-6 p-1 brand-container">
 		<a class="brand" href="<?= esc_url(home_url('/')); ?>">
 			  <img class="logo" src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
