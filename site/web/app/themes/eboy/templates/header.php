5bfb74ea54d06331b5924bd28be5da5903cd0bac
eboy <giannisduke@gmail.com>
2018-11-24 10:36:51 +0200

    eboy: header on mobile

--- site/web/app/themes/eboy/templates/header.php
+++ site/web/app/themes/eboy/templates/header.php
@@ -1,6 +1,7 @@
 
-	<header class="d-flex flex-wrap justify-content-between px-5 py-3">
-  <div class="col-12 col-lg-6 p-1 brand-container">
+	<header class="d-flex flex-row justify-content-between px-5 py-3">
+
+  <div class="col-6 col-lg-6 p-1 brand-container">
 		<a class="brand" href="<?= esc_url(home_url('/')); ?>">
 			  <img class="logo" src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
 		</a>
@@ -12,7 +13,7 @@
 		  'theme_location' => 'primary',
 		  'walker' => new Microdot_Walker_Nav_Menu(),
 		  'container' => false,
-		  'items_wrap' => '<div class="col-12 col-lg-6 text-right p-0 menu-area">%3$s</div>'
+		  'items_wrap' => '<div class="col-6 col-lg-6 text-right p-0 menu-area">%3$s</div>'
 		));
 		?>
 
--- site/web/app/themes/eboy/templates/unit-hero_static.php
+++ site/web/app/themes/eboy/templates/unit-hero_static.php
@@ -1,5 +1,5 @@
-  <section id="top" class="bg-white top mt-2">
-    <div class="header-content container-fluid">
+  <section id="top" class="bg-white top mt-5">
+    <div class="header-content container-fluid px-5">
       <div class="row">
         <div class="col-12 p-0">
           <h1 ><?= get_post_field('post_title', $post->ID) ?></h1>
