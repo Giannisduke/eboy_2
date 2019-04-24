8b788ec35af78554d37d61230a0d5d176c461f2e
eboy <giannisduke@gmail.com>
2019-04-24 16:42:11 +0300

    Loukia - slide left ok

--- site/web/app/themes/loukia/base.php
+++ site/web/app/themes/loukia/base.php
@@ -20,7 +20,7 @@ use Roots\Sage\Wrapper;
     ?>
     <div class="wrap container-fluid" role="document">
       <div class="content row">
-        <main class="main p-0">
+        <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
         <?php if (Setup\display_sidebar()) : ?>
