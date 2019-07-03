e2ed3e6186a2975ef7b6e040e076be1c5488399b
eboy <giannisduke@gmail.com>
2019-07-03 20:43:27 +0300

    paidikarouxaonline - hide sidebar on category pages

--- site/web/app/themes/paidikarouxaonline/lib/setup.php
+++ site/web/app/themes/paidikarouxaonline/lib/setup.php
@@ -121,6 +121,8 @@ function display_sidebar() {
     is_front_page(),
     is_page_template('template-custom.php'),
     is_shop(),
+    is_product(),
+  
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
