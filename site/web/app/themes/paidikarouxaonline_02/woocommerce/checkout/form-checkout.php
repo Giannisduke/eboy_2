b17075cae13e241abba7bfbd9cb00f05af95df16
eboy <giannisduke@gmail.com>
2019-08-21 18:18:50 +0300

    paidikarouxaonline - checkout

--- site/web/app/themes/paidikarouxaonline_02/woocommerce/checkout/form-checkout.php
+++ site/web/app/themes/paidikarouxaonline_02/woocommerce/checkout/form-checkout.php
@@ -30,39 +30,43 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 ?>
 
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-
+<div class="container">
+	<div class="row">
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
 
+<div class="col-sm-6">
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<div class="col2-set" id="customer_details">
+
 
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 
 
-	
+
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 
-		</div>
+
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+</div>
+<div class="col-sm-6">
 
 
-
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+	<?php do_action( 'woocommerce_checkout_before_order_review'); ?>
 
 	<div id="order_review" class="woocommerce-checkout-review-order">
+
 		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 	</div>
 
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+</div>
 
+</div> <!-- row. End -->
+</div> <!-- container. End -->
 </form>
-
-<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
+		<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
