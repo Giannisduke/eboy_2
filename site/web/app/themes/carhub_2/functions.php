bfa1cb4edd1e6e4d3ce03efd0b6436aab7680ec4
eboy <giannisduke@gmail.com>
2019-10-31 11:36:02 +0200

    Carhub - Dates ok

--- site/web/app/themes/carhub_2/functions.php
+++ site/web/app/themes/carhub_2/functions.php
@@ -366,8 +366,5 @@ add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
 /* Remove Related Products */
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 
-add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
-
-function remove_add_to_cart_buttons() {
-    remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
-}
+/* Remove Categories from Single Products */
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
