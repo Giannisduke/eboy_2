f3796d78eab64bb79368dae3ba2822588e592ca6
Giannis <giannis@eboy.gr>
2020-08-13 23:32:21 +0300

    Update template-eboy_v3.php

--- site/web/app/themes/eboy_v3/template-eboy_v3.php
+++ site/web/app/themes/eboy_v3/template-eboy_v3.php
@@ -3,7 +3,6 @@
  * Template Name: Front eboy_v3 Template
  */
 ?>
-
 <?php get_template_part('templates/unit', 'hero_static'); ?>
 <?php // get_template_part('templates/unit', 'about'); ?>
 <?php get_template_part('templates/unit', 'isotope'); ?>
